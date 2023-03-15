5ce1e4cc2de2f1f3e4ee9e37b3fc325e1ceece79
yash <yash224.yr@gmail.com>
2023-03-15 17:09:51 +0530

    commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Routing from './ContextApi/Routing';
+// import Routing from './ContextApi/Routing';
 // import Movies from './MoviesWeb/Movies';
 // import Routing from './Routing/routing';
 // import Cart from './Cart/cart';
 // import Axios from './Axios/Axios';
 // import {Todo} from "./To-do/Todo"
-// import Tesla from "./Tesla/Tesla"
+import Tesla from "./Tesla/Tesla"
 // import Tabs from "./button/tabs"
 
 
@@ -17,13 +17,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
    // <Todo/>                                   
-   // <Tesla/>
+   <Tesla/>
    // <Axios/>
    // <Tabs/>
    // <Cart/>
    // <Routing/>
    // <Movies/>
-   <Routing/>
+   // <Routing/>
 );
 
 
